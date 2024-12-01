dd940ed45a2b0dc36217dd5f0bff4d3b067a1881
Sk Aqib Ali <122338420+Aqib-Ali08@users.noreply.github.com>
2024-12-01 12:04:04 +0530

    changed link

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ const app = express();
 const server = http.createServer(app); // Create HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "https://webrtc-video-app-9kfp.onrender.com", // Allow your frontend origin
+    origin: "https://webrtc-video-app-1.onrender.com", // Allow your frontend origin
     methods: ["GET", "POST"],
   },
 });
 
 // Configure CORS for Express
 app.use(cors({
-  origin: "https://webrtc-video-app-9kfp.onrender.com", // Allow your frontend origin
+  origin: "https://webrtc-video-app-1.onrender.com", // Allow your frontend origin
 }));
 app.use(bodyParser.json());
 
