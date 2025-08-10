09b4e150f576b1873ab47c974e9a82707bf22c06
Sk Aqib Ali <122338420+Aqib-Ali08@users.noreply.github.com>
2025-08-10 12:26:54 +0530

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,8 +5,8 @@ import authenticateToken from '../middlewares/authenticateToken.middleware.js';
 const router = express.Router();
 
 // router.get('/getAllUsers', authenticateToken, getAllUsers);
-router.get('/list_other_users', authenticateToken, list_other_users);
-router.get('/list_recieved_requests', authenticateToken, list_recieved_requests);
-router.get('/list_connected_users', authenticateToken, list_connected_users);
+router.post('/list_other_users', authenticateToken, list_other_users);
+router.post('/list_recieved_requests', authenticateToken, list_recieved_requests);
+router.post('/list_connected_users', authenticateToken, list_connected_users);
 
 export default router;
